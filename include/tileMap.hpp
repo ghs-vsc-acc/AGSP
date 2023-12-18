7aa40d295a3a472e023fff675e4235d3fa3aa236
ghs-vsc-acc <144269646+ghs-vsc-acc@users.noreply.github.com>
2023-12-18 12:46:31 -0500

    [N/A]: ..

--- include/tileMap.hpp
+++ include/tileMap.hpp
@@ -1,5 +1,35 @@
 #pragma once
 
+/*
+NOTE: consider that "levels" need to be implemented, in map have levels then in levels have tiles (for later)
+example theoretical manifest file:
+
+{
+	"map": {
+		"tiles": [
+			{
+				"textureId": 1,
+				"x": 0,
+				"y": 0,
+				"properties": {
+					"passable": false, // like can you walk through this object, is it solid
+				}
+			}
+		],
+		"textures": [
+			{
+				"id": 1,
+				"file": "skyTextureBar.png"
+			},
+			{
+				"id": 2,
+				"file": "groundTextureFoo.png"
+			}
+		]
+	}
+}
+*/
+
 struct Tile {};
 
 class TileMap {}; // see if should struct
