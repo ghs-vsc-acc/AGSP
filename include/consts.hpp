29cfbc94244b4412163f66aadd2b1f9b3ba82454
ghs-vsc-acc <144269646+ghs-vsc-acc@users.noreply.github.com>
2024-01-05 13:49:40 -0500

    [N/A]: ..

--- include/consts.hpp
+++ include/consts.hpp
@@ -1,8 +1,8 @@
 #ifndef CONSTS_HPP
 #define CONSTS_HPP
 
-constexpr int SCREEN_SIZE = 800;
-constexpr int GRID_SIZE = 16; // 32x32
-constexpr int TILE_SIZE = (SCREEN_SIZE / GRID_SIZE); // 50x50
+constexpr int SCREEN_SIZE = 800; // comment out
+constexpr int GRID_SIZE = 16; // 32x32 // original: 32
+constexpr int TILE_SIZE = (SCREEN_SIZE / GRID_SIZE); // 50x50 // original: 32
 
 #endif // CONSTS_HPP
